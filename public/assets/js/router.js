9c85a5fc2deceabf7e42f7275f2681d2124cdf41
josiahprathaban <josiah.prathaban@gmail.com>
2023-09-13 15:29:02 +0530

    auguest 2023 added

--- public/assets/js/router.js
+++ public/assets/js/router.js
@@ -2,6 +2,9 @@ const routes = {
   404: "pages/404.html",
   "": "pages/index.html",
   "#2023-july": "pages/2023-july.html",
+  "#2023-august": "pages/2023-august.html",
+  "#2022-may": "pages/2022-may.html",
+  "#2021-november": "pages/2021-november.html",
 };
 
 const handleRouteChange = async () => {
